7085dc22624cb556bde6a0adde954571d4a624a1
wcchin <wcchin.88@gmail.com>
2024-01-02 18:08:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sg_health_access/dist/',
+  base: '/sg_health_access/',
   plugins: [react()],
 }) 
