231ef394a15ed64973251e6e0658fab39feaebd5
wcchin <wcchin.88@gmail.com>
2024-01-03 01:46:10 +0800

    update some text

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/sg_health_access/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested1: resolve(__dirname, 'flowmapgl_radiation_clinic/index.html'),
+        nested2: resolve(__dirname, 'keplergl-accessibility_clinic/index.html'),
+      },
+    },
+  },
 }) 
