6041e81e4872a70234e22c88b885bb51acada723
wcchin <wcchin.88@gmail.com>
2024-01-05 10:59:45 +0800

    change toggle menu

--- js/this_site.js
+++ js/this_site.js
@@ -1,4 +1,4 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -8,4 +8,22 @@ window.onscroll = function() {
     document.getElementById("navbar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
-} 
+} */
+
+var nav = document.getElementById('nav');
+var navlinks = nav.getElementsByTagName('a');
+
+function toggleNav() {
+    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
+  }
+
+document.getElementById('nav-icon').addEventListener('click', function(e) {
+    e.preventDefault();
+    toggleNav();
+});
+
+for(var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener('click', function() {
+      toggleNav();
+  });
+}
