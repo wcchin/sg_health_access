da638eb1c7716babce94a2ecea3ca796fd27bcdd
wcchin <wcchin.88@gmail.com>
2024-01-05 23:16:37 +0800

    update menu

--- js/this_site.js
+++ js/this_site.js
@@ -1,29 +1,61 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-} */
 
-var nav = document.getElementById('nav');
+var nav = document.getElementById('mySidenav');
 var navlinks = nav.getElementsByTagName('a');
+var navicon = document.getElementById('nav-icon');
 
+/*
 function toggleNav() {
-    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
+    //(nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
+    if (nav.classList.contains('active')) {
+      nav.classList.remove('active');
+      closeNav();
+    } else {
+      nav.classList.add('active');
+      openNav();
+    }
   }
+*/
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  nav.style.width = "250px";
+  nav.classList.add('active');
+  //navicon.classList.add('active');
+  navicon.style.display = "none";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  nav.style.width = "0";
+  nav.classList.remove('active');
+  //navicon.classList.remove('active');
+  //navicon.style.width = "45px";
+  navicon.style.display = "inline-block";
+} 
 
 document.getElementById('nav-icon').addEventListener('click', function(e) {
     e.preventDefault();
-    toggleNav();
+    //toggleNav();
+    openNav();
 });
 
 for(var i = 0; i < navlinks.length; i++) {
-    navlinks[i].addEventListener('click', function() {
-      toggleNav();
+  navlinks[i].addEventListener('click', function() {
+    closeNav();
   });
 }
+
+document.addEventListener('keydown', (event) => {
+  //const e = event || window.event;
+  if (event.key === "Escape") { // Escape key
+    closeNav();
+  }
+});
+
+document.addEventListener('click', function (event) {
+  
+  if (!event.target.closest('#mySidenav') && !event.target.closest('#nav-icon')) {
+    closeNav();
+  }
+}, false);
+/**/
